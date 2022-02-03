582a4363614df076acd2d7b76f65acf792efa412
rycont <reactdev@kakao.com>
2022-02-03 11:05:02 +0900

    chore: add brand asset

---
+++ @/assets/index.ts
@@ -0,0 +1,1 @@
+export { default as logo } from "./brand/horizontal.svg"
---
+++ @/components/LoadSVG.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export const LoadSVG: React.FC<{
+    width: number
+    height: number
+    src: string
+    alt: string
+}> = (props) => {
+    return (
+        <div
+            style={{
+                position: "relative",
+                minHeight: props.height + "rem",
+                minWidth: props.width + "rem",
+            }}
+        >
+            <Image src={props.src} layout="fill" alt={props.alt} />
+        </div>
+    )
+}

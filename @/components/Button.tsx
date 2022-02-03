53ac5f851ca8c271f514adb3103da0762211f1a3
rycont <reactdev@kakao.com>
2022-02-03 11:04:13 +0900

    feat: implement button component

---
+++ @/components/Button.tsx
@@ -0,0 +1,25 @@
+import { styled } from "@/stitches.config"
+
+export const Button = styled("button", {
+    backgroundColor: "$accent",
+    border: "none",
+    outline: "none",
+    padding: "2rem 4rem",
+    borderRadius: "2rem",
+    color: "white !important",
+    gap: "2rem",
+    alignSelf: "stretch",
+    high: true,
+    "&:focus": {
+        boxShadow:
+            "0px 1rem 2rem 0px #2EA4AB5C, inset 0rem 0rem 0px 0.5rem black",
+    },
+    variants: {
+        big: {
+            true: {
+                padding: "4rem 6rem",
+                gap: "3rem",
+            },
+        },
+    },
+})
---
+++ @/components/index.ts
@@ -0,0 +1,3 @@
+export * from "./LoadSVG"
+export * from "./Input"
+export * from "./Button"

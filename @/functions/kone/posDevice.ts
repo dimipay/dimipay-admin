07c22e26a88719ac83e806d158395095bde4d9da
정한 <35295182+rycont@users.noreply.github.com>
2022-02-23 04:08:26 +0000

    chore: remove * export in page route

--- @/functions/kone/posDevice.ts
+++ @/functions/kone/posDevice.ts
@@ -1,4 +1,4 @@
-import { otpKone } from "pages/api/pos-device"
+import { otpKone } from "pages/api/pos-device/create-otp"
 import { kone } from "./core"
 
 export const createOtp = kone<otpKone["POST"]>("pos-device/create-otp", "POST")

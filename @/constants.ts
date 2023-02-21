cd438fe6686b2d41286cccc1398ffd1043406bf5
Tilto_ <tilto0822@gmail.com>
2023-02-21 10:59:34 +0900

    chore: disable notion logging

--- @/constants.ts
+++ @/constants.ts
@@ -25,8 +25,8 @@ export const assert = (name: string, isServersideOnly = true) => {
 
 export const JWT_SECRET = assert("JWT_SECRET")
 export const REDIS_URI = assert("REDIS_URI")
-export const NOTION_TOKEN = assert("NOTION_TOKEN")
-export const NOTION_TABLE = assert("NOTION_TABLE")
+// export const NOTION_TOKEN = assert("NOTION_TOKEN")
+// export const NOTION_TABLE = assert("NOTION_TABLE")
 
 export const SOFT_DELETE_FIELD_NAME = "is_deleted"
 
--- @/functions/logNotion.ts
+++ @/functions/logNotion.ts
@@ -1,4 +1,4 @@
-import { NOTION_TABLE, NOTION_TOKEN } from "@/constants"
+// import { NOTION_TABLE, NOTION_TOKEN } from "@/constants"
 import axios from "axios"
 
 export const logNotion = async (props: {
@@ -8,74 +8,73 @@ export const logNotion = async (props: {
     data: any
     result: any
 }) => {
-    const result = JSON.stringify(props.result)?.slice(0, 2000)
-
-    try {
-        await axios("https://api.notion.com/v1/pages", {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${NOTION_TOKEN}`,
-                "Notion-Version": "2022-02-22",
-            },
-            data: {
-                parent: {
-                    database_id: NOTION_TABLE,
-                },
-                properties: {
-                    행위자: {
-                        select: {
-                            name: props.actioner,
-                        },
-                    },
-                    유형: {
-                        select: {
-                            name: props.type,
-                        },
-                    },
-                    주소: {
-                        rich_text: [
-                            {
-                                text: {
-                                    content: JSON.stringify(props.url)?.slice(
-                                        0,
-                                        2000,
-                                    ),
-                                },
-                            },
-                        ],
-                    },
-                    내용: {
-                        rich_text: [
-                            {
-                                text: {
-                                    content: JSON.stringify(props.data)?.slice(
-                                        0,
-                                        2000,
-                                    ),
-                                },
-                            },
-                        ],
-                    },
-                    결과: {
-                        rich_text: [
-                            {
-                                text: {
-                                    content: result.includes("eyJ")
-                                        ? "BLIND"
-                                        : result,
-                                },
-                            },
-                        ],
-                    },
-                    환경: {
-                        select: {
-                            name: process.env.NODE_ENV,
-                        },
-                    },
-                },
-            },
-        })
-    } catch (e) {
-        console.error(e)
-    }
+    // const result = JSON.stringify(props.result)?.slice(0, 2000)
+    // try {
+    //     await axios("https://api.notion.com/v1/pages", {
+    //         method: "POST",
+    //         headers: {
+    //             Authorization: `Bearer ${NOTION_TOKEN}`,
+    //             "Notion-Version": "2022-02-22",
+    //         },
+    //         data: {
+    //             parent: {
+    //                 database_id: NOTION_TABLE,
+    //             },
+    //             properties: {
+    //                 행위자: {
+    //                     select: {
+    //                         name: props.actioner,
+    //                     },
+    //                 },
+    //                 유형: {
+    //                     select: {
+    //                         name: props.type,
+    //                     },
+    //                 },
+    //                 주소: {
+    //                     rich_text: [
+    //                         {
+    //                             text: {
+    //                                 content: JSON.stringify(props.url)?.slice(
+    //                                     0,
+    //                                     2000,
+    //                                 ),
+    //                             },
+    //                         },
+    //                     ],
+    //                 },
+    //                 내용: {
+    //                     rich_text: [
+    //                         {
+    //                             text: {
+    //                                 content: JSON.stringify(props.data)?.slice(
+    //                                     0,
+    //                                     2000,
+    //                                 ),
+    //                             },
+    //                         },
+    //                     ],
+    //                 },
+    //                 결과: {
+    //                     rich_text: [
+    //                         {
+    //                             text: {
+    //                                 content: result.includes("eyJ")
+    //                                     ? "BLIND"
+    //                                     : result,
+    //                             },
+    //                         },
+    //                     ],
+    //                 },
+    //                 환경: {
+    //                     select: {
+    //                         name: process.env.NODE_ENV,
+    //                     },
+    //                 },
+    //             },
+    //         },
+    //     })
+    // } catch (e) {
+    //     console.error(e)
+    // }
 }
